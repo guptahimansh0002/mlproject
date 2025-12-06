547ed5eccb9538c2b5a3817792e433dfcc3e94a9
Himanshu Gupta <himanshugupta5704@gmailcom>
2025-12-07 01:07:22 +0530

    recorrect

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,12 @@
 import sys
-
+import os
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
+    file_name=os.path.relpath(file_name)
+    
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
 
@@ -17,3 +19,4 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+
