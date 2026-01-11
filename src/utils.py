d591232f3068739368c2f963198e91d6f2e3d488
Himanshu Gupta <himanshugupta5704@gmailcom>
2026-01-11 16:26:42 +0530

    Utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
-from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import r2_score
+# from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
